4d77a6e944bf7c472d368ad47b1261a0a70bfa2d
aakashrjrajpoot12 <aakashrjrajpoot@gmail.com>
2023-02-23 12:26:00 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
+export default class App extends React.Component {
+  constructor(props){       //constructor
+    super(props);
+    this.state={            //state object
+      error:null,
+      isLoaded:false,
+      items:[],
+    };
+  }
+}
+componentDidMount(){
+  fetch('items.json')          //you can also use Actual url of API
+  .then((res)=>res.json())
+  .then((result)=>{this.setState({isLoaded=true,items:result.items,});
+},
+)
+}
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
