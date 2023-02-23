fa1a881198f0e961f889565cec994085c9f62f49
aakashrjrajpoot12 <aakashrjrajpoot@gmail.com>
2023-02-23 14:25:01 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -10,44 +10,46 @@ export default class App extends React.Component {
       items:[],
     };
   }
-}
+
 componentDidMount(){
   fetch('items.json')          //you can also use Actual url of API
   .then((res)=>res.json())//callback function
   
   .then(
     (result)=>{
-      this.setState({isLoaded=true,items:result.items,});//statechange
+      this.setState({isLoaded:true,
+        items:result.items,});//statechange
 },
 (error)=>{
   this.setState({
-    isLoaded=true,
+    isLoaded:true,
     error,
   });
 }
   );
 }
-render()
-{
-  const{error,isLoaded,items}=this.state;
+render(){
+  const { error,isLoaded,items}=this.state;
   if(error){
     return<div>Error:{error.message}</div>;
   }
-  elseif(!isLoaded){
+  else if(!isLoaded){
     return<div>Loading...</div>;
   }
-  else{
-    {
-      return(
-        <ul>{items.map((item)=>(<li key={item.id}>
+  else
+  {
+    return(
+        <ul>
+          {items.map((item)=>
+        (<li key={item.id}>
           {item.name}{item.price}
           </li>
           ))}
           </ul>
       );
       
-    
   }
   }
-}
+  }
+
   
